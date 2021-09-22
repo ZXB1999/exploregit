ddda5d65bf6541d209e1700aac9407196e89ebb3
ttmy <1104165130@qq.com>
2021-09-22 21:39:27 +0800

    初始化提交项目

---
+++ src/main/java/com/ttmy/exploregit/ExploregitApplication.java
@@ -0,0 +1,13 @@
+package com.ttmy.exploregit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploregitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploregitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttmy/exploregit/ExploregitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttmy.exploregit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploregitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
